b5ea65485a769bd1b3c64845973c0906f44e1574
swirvyiswack <swirvy1@proton.me>
2025-06-27 21:57:05 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hey! I'm online and working.")
+    
+bot.run(TOKEN)
